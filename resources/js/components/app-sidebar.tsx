5781f1d5a94bf1e7ba67a5f721579f9250124475
Gilbert Engalan <gilbertpude2003@gmail.com>
2026-02-09 00:59:42 +0800

    add feat

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -1,5 +1,5 @@
-import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-react';
+import { Link, usePage } from '@inertiajs/react';
+import { Bed, BookImageIcon, BookOpen, Folder, LayoutGrid, User } from 'lucide-react';
 import { NavFooter } from '@/components/nav-footer';
 import { NavMain } from '@/components/nav-main';
 import { NavUser } from '@/components/nav-user';
@@ -13,16 +13,49 @@ import {
     SidebarMenuItem,
 } from '@/components/ui/sidebar';
 import { dashboard } from '@/routes';
-import type { NavItem } from '@/types';
+import type { NavItem, SharedData } from '@/types';
 import AppLogo from './app-logo';
 
-const mainNavItems: NavItem[] = [
+const managerNavItems: NavItem[] = [
     {
         title: 'Dashboard',
         href: dashboard(),
         icon: LayoutGrid,
     },
+    {
+        title: 'Rooms',
+        href: '/rooms',
+        icon: Bed,
+    },
+    {
+        title: 'Guests',
+        href: '/guests',
+        icon: User,
+    },
+    {
+        title: 'Bookings',
+        href: '/bookings',
+        icon: BookImageIcon,
+    },
 ];
+
+const adminNavItem: NavItem[] = [
+    {
+        title: 'Dashboard',
+        href: dashboard(),
+        icon: LayoutGrid,
+    },
+    {
+        title: 'Assign Manager',
+        href: '/assign-manager',
+        icon: User,
+    },
+    {
+        title: 'Manage Hotel',
+        href: '/hotels',
+        icon: User,
+    },
+]
 
 const footerNavItems: NavItem[] = [
     {
@@ -38,6 +71,46 @@ const footerNavItems: NavItem[] = [
 ];
 
 export function AppSidebar() {
+
+    const { auth } = usePage<SharedData>().props;
+    const user = auth?.user;
+
+    if (user?.role === 'manager' && !user?.tenant_id) {
+        return (
+            <Sidebar collapsible="icon" variant="inset">
+                <SidebarHeader>
+                    <SidebarMenu>
+                        <SidebarMenuItem>
+                            <SidebarMenuButton size="lg" asChild>
+                                <Link href={dashboard()} prefetch>
+                                    <AppLogo />
+                                </Link>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    </SidebarMenu>
+                </SidebarHeader>
+
+                <SidebarContent>
+                    <NavMain items={
+                        [
+                            {
+                                title: "Dashboard",
+                                href: '/dashboard',
+                                icon: LayoutGrid
+                            }
+                        ]
+                    } />
+                </SidebarContent>
+
+                <SidebarFooter>
+                    <NavFooter items={footerNavItems} className="mt-auto" />
+                    <NavUser />
+                </SidebarFooter>
+            </Sidebar>
+        );
+    }
+
+    const isAdmin = auth?.user?.role === 'admin';
     return (
         <Sidebar collapsible="icon" variant="inset">
             <SidebarHeader>
@@ -53,7 +126,9 @@ export function AppSidebar() {
             </SidebarHeader>
 
             <SidebarContent>
-                <NavMain items={mainNavItems} />
+                <NavMain items={
+                    isAdmin ? adminNavItem: managerNavItems
+                } />
             </SidebarContent>
 
             <SidebarFooter>
